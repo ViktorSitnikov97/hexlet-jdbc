775f4867955bf5ee9625b2b4afd6c9d047a96e5a
Viktor Sitnikov <sitnikoff_victor_97@mail.ru>
2024-08-10 17:38:27 +0300

    Update Main.java and UserDAO.java

--- app/src/main/java/io/hexlet/Main.java
+++ app/src/main/java/io/hexlet/Main.java
@@ -12,17 +12,79 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) throws SQLException {
 
-        try (Connection conn = DriverManager.getConnection("jdbc:h2:mem:hexlet_test")) {
-            var sql1 = "CREATE TABLE users (id BIGINT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(255), phone VARCHAR(255))";
+        try (Connection conn = DriverManager.getConnection("jdbc:h2:mem:test_db")) {
 
-            try (Statement statement1 = conn.createStatement()) {
-                statement1.execute(sql1);
+            try (var statement = conn.createStatement()) {
+                String sql = "CREATE TABLE users (" +
+                        "id BIGINT PRIMARY KEY AUTO_INCREMENT," +
+                        "name VARCHAR(255) NOT NULL," +
+                        "phone VARCHAR(255) NOT NULL UNIQUE" +
+                        ");";
+                if (!statement.execute(sql)) {
+                    System.out.println("Database with users table has been created!");
+                }
             }
 
-            var sql2 = "INSERT INTO users (username, phone) VALUES ('tommy', '123456789')";
-            try (Statement statement2 = conn.createStatement()) {
-                statement2.executeUpdate(sql2);
-            }
+            UserDAO dao = new UserDAO(conn);
+
+            User user1 = new User("Anya", "+79153424063");
+            User user2 = new User("Alyona", "+79156574073");
+            dao.save(user1);
+            dao.save(user2);
+            System.out.println(dao.find(user1.getId()));
+            System.out.println(dao.find(user2.getId()));
+
+            User user3 = new User("Svetlana", "+79683422223");
+            System.out.println(dao.find(user3.getId()));
+            dao.save(user3);
+            System.out.println(dao.find(user3.getId()));
+
+            User user4 = new User("Damir", "+79683999099");
+            User user5 = new User("John", "+79993422222");
+            dao.save(user4);
+            dao.save(user5);
+            System.out.println(dao.find(user4.getId()));
+            System.out.println(dao.find(user5.getId()));
+            user4.setName("DamirNew");
+            user5.setPhone("00000000000");
+            dao.save(user4);
+            dao.save(user5);
+            System.out.println(dao.find(user4.getId()));
+            System.out.println(dao.find(user5.getId()));
+
+            User user10 = new User("Lola", "1337");
+            System.out.println(dao.find(user10.getId()));
+            dao.delete(user10);
+
+            dao.delete(user3);
+            System.out.println(dao.find(user3.getId()));
+            System.out.println(dao.find(Long.valueOf(3)));
+
+
+
+
+
+
+        }
+
+
+    }
+}
+
+//public class Application {
+    //    public static void main(String[] args) throws SQLException {
+//
+//        try (Connection conn = DriverManager.getConnection("jdbc:h2:mem:hexlet_test")) {
+//            var sql1 = "CREATE TABLE users (id BIGINT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(255), phone VARCHAR(255))";
+
+//            try (Statement statement1 = conn.createStatement()) {
+//                statement1.execute(sql1);
+//            }
+//
+//            var sql2 = "INSERT INTO users (username, phone) VALUES ('tommy', '123456789')";
+//            try (Statement statement2 = conn.createStatement()) {
+//                statement2.executeUpdate(sql2);
+//            }
 
 //            var sql3 = "SELECT * FROM users";
 //            try (Statement statement3 = conn.createStatement()) {
@@ -60,58 +122,58 @@ public class Main {
 //                }
 //
 //            }
-            var sql5 = "SELECT * FROM users";
-            try (PreparedStatement preparedStatement2 = conn.prepareStatement(sql5)) {
-                ResultSet result = preparedStatement2.executeQuery();
-
-                while(result.next()){
-                    String name = result.getString("username");
-                    String phone = result.getString("phone");
-                    System.out.printf("%s %s\n",name,phone);
-                }
-            }
-            Map<String, String> users = Map.of(
-                    "Sonny", "111111",
-                    "Artem", "2222222",
-                    "Misha", "33333333",
-                    "Alex", "44444444");
-            var sql6 = "INSERT INTO users (username, phone) VALUES (?, ?)";
-            try (var preparedStatement = conn.prepareStatement(sql6, Statement.RETURN_GENERATED_KEYS)) {
-                for (var user : users.entrySet()) {
-                    var key = user.getKey();
-                    var value = user.getValue();
-                    preparedStatement.setString(1, key);
-                    preparedStatement.setString(2, value);
-                    preparedStatement.executeUpdate();
-
-                    ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
-                    if (generatedKeys.next()) {
-                        System.out.println(generatedKeys.getLong(1));
-                    } else {
-                        throw new SQLException("DB have not returned an id after saving the entity");
-                    }
-                }
-            }
-            List<String> deleteUser = List.of("Artem", "Misha");
-            var sql7 = "DELETE FROM users WHERE username = ?";
-            try (PreparedStatement preparedStatement1 = conn.prepareStatement(sql7)) {
-                for (var username : deleteUser) {
-                    preparedStatement1.setString(1, username);
-                    preparedStatement1.executeUpdate();
-                }
+//            var sql5 = "SELECT * FROM users";
+//            try (PreparedStatement preparedStatement2 = conn.prepareStatement(sql5)) {
+//                ResultSet result = preparedStatement2.executeQuery();
 //
-            }
-            var sql8 = "SELECT * FROM users WHERE id > ?";
-            try (PreparedStatement preparedStatement2 = conn.prepareStatement(sql8)) {
-                preparedStatement2.setInt(1, 0);
-                ResultSet result = preparedStatement2.executeQuery();
-                while(result.next()){
-                    int id = result.getInt("id");
-                    String name = result.getString("username");
-                    String phone = result.getString("phone");
-                    System.out.printf("%d %s %s\n", id, name, phone);
-                }
-            }
-        }
-    }
-}
+//                while(result.next()){
+//                    String name = result.getString("username");
+//                    String phone = result.getString("phone");
+//                    System.out.printf("%s %s\n",name,phone);
+//                }
+//            }
+//            Map<String, String> users = Map.of(
+//                    "Sonny", "111111",
+//                    "Artem", "2222222",
+//                    "Misha", "33333333",
+//                    "Alex", "44444444");
+//            var sql6 = "INSERT INTO users (username, phone) VALUES (?, ?)";
+//            try (var preparedStatement = conn.prepareStatement(sql6, Statement.RETURN_GENERATED_KEYS)) {
+//                for (var user : users.entrySet()) {
+//                    var key = user.getKey();
+//                    var value = user.getValue();
+//                    preparedStatement.setString(1, key);
+//                    preparedStatement.setString(2, value);
+//                    preparedStatement.executeUpdate();
+//
+//                    ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
+//                    if (generatedKeys.next()) {
+//                        System.out.println(generatedKeys.getLong(1));
+//                    } else {
+//                        throw new SQLException("DB have not returned an id after saving the entity");
+//                    }
+//                }
+//            }
+//            List<String> deleteUser = List.of("Artem", "Misha");
+//            var sql7 = "DELETE FROM users WHERE username = ?";
+//            try (PreparedStatement preparedStatement1 = conn.prepareStatement(sql7)) {
+//                for (var username : deleteUser) {
+//                    preparedStatement1.setString(1, username);
+//                    preparedStatement1.executeUpdate();
+//                }
+////
+//            }
+//            var sql8 = "SELECT * FROM users WHERE id > ?";
+//            try (PreparedStatement preparedStatement2 = conn.prepareStatement(sql8)) {
+//                preparedStatement2.setInt(1, 0);
+//                ResultSet result = preparedStatement2.executeQuery();
+//                while(result.next()){
+//                    int id = result.getInt("id");
+//                    String name = result.getString("username");
+//                    String phone = result.getString("phone");
+//                    System.out.printf("%d %s %s\n", id, name, phone);
+//                }
+//            }
+//        }
+//    }
+//}
--- app/src/main/java/io/hexlet/UserDAO.java
+++ app/src/main/java/io/hexlet/UserDAO.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Optional;
 
 public class UserDAO {
@@ -17,7 +18,7 @@ public class UserDAO {
 
         if (user.getId() == null) {
             String sql1 = "INSERT INTO users (name, phone) VALUES (? , ?);";
-            try (PreparedStatement preparedStatement1 = connection.prepareStatement(sql1)) {
+            try (PreparedStatement preparedStatement1 = connection.prepareStatement(sql1, Statement.RETURN_GENERATED_KEYS)) {
                 preparedStatement1.setString(1, user.getName());
                 preparedStatement1.setString(2, user.getPhone());
                 preparedStatement1.executeUpdate();
@@ -48,20 +49,18 @@ public class UserDAO {
             try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
                 preparedStatement.setLong(1, user.getId());
                 preparedStatement.executeUpdate();
-
-                ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
-                if (generatedKeys.next()) {
-                    long id = generatedKeys.getLong(1);
-                    System.out.println("ID = " + id);
-                    System.out.println(user + "has been deleted");
+                System.out.println(user + "has been deleted");
+                user.setId(null);
                 }
-            }
-        } else {
-            throw new SQLException("There is no such id in the users table");
+            } else {
+            System.out.println("There is no such user in the users table");
         }
     }
 
     public Optional<User> find(Long id) throws SQLException {
+        if (id == null) {
+            return Optional.empty();
+        }
         String sql = "SELECT * FROM users WHERE id = ?;";
         try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             preparedStatement.setLong(1, id);
